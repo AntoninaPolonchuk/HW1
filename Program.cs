3b69f02704dd73863153bd41af119dfc38dc7c00
Antonina <polonchuk.a@gmail.com>
2022-07-20 18:48:56 +0300

    Добавила метод, который учеличивает число на 10

--- Program.cs
+++ Program.cs
@@ -8,8 +8,24 @@ namespace HW_2._1
 {
     internal class Program
     {
+
+
+        static int Value(int number)
+        {
+            return number + 10;
+        }
+
+
         static void Main(string[] args)
         {
+
+                Console.WriteLine("введите число");
+                int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Value(a));
+                Console.ReadKey();  
         }
     }
 }
+
+
